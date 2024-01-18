2c10413787dc4220af0e8fee17d1a4aadc2b4287
temagt250 <132059934+temagt250@users.noreply.github.com>
2024-01-18 16:57:32 +0300

    mainColab.py to change

--- mainColab.py
+++ mainColab.py
@@ -19,5 +19,3 @@ data.set_index([data.index, 'whoAmI'], inplace=True)
 data = data.unstack(level=-1, fill_value = 0).astype(int)
 
 data.columns = data.columns.droplevel()
-
-data.columns.name = None
